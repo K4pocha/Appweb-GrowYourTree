707c48eb42635438ec5de678519b0c102c1f97e0
Nicolas Oñate <k4pocha@gmail.com>
2024-04-21 22:57:44 -0400

    WebBrowser icon added

--- old/js22/auth.js
+++
@@ -1,8 +0,0 @@
-//JS Para todas las paginas que requieran de autenticación del usuario para acceder.
-
-document.addEventListener("DOMContentLoaded", function() {
-    if (!localStorage.getItem('userEmail')) {
-        alert("Debes iniciar sesión para acceder a esta página.");
-        window.location.href = "index.html"; // Redirecciona a la página de inicio de sesión
-    }
-});
